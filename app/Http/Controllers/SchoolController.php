9f585619b5f942ad699ff3592c1d21ea249c3197
csengekulker <k71608883429@ktch.hu>
2022-10-20 10:22:02 +0200

    filter students by course id

--- app/Http/Controllers/SchoolController.php
+++ app/Http/Controllers/SchoolController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Course;
 use App\Models\Student;
 
 class SchoolController extends Controller
@@ -12,14 +13,25 @@ class SchoolController extends Controller
         // store result
         $students = Student::with( "course" )->get();
 
-        // foreach( $students as $student ) {
-        //     echo "<pre>";
-        //     print_r($student->name." | ".$student->course->course);
-        // }
+        foreach( $students as $student ) {
+            echo "<pre>";
+            print_r($student->name." | ".$student->course->course);
+        }
 
         return view("students", [
             "students" => $students
         ]);
 
+    }
+
+    public function filterStudents() {
+
+        //store filtered result
+        // course with Id 3 belongs to which student
+        $students = Course::find(2)->student;
+
+        return view("students", [
+            "students" => $students
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 });
 
 
-Route::get('/students', [ SchoolController::class, 'listStudents']);
+Route::get('/students', [ SchoolController::class, 'filterStudents']);
